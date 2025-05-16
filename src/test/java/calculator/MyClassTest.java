33b162f653aad4d89e51e6a03bdd02c0ef9c3e4c
Takikato09 <liandelacruz09@hotmail.com>
2025-05-16 19:18:53 +0200

    Introduce failing test on purpose the second time

--- src/test/java/calculator/MyClassTest.java
+++ src/test/java/calculator/MyClassTest.java
@@ -7,8 +7,10 @@ import static org.junit.Assert.assertEquals;
 public class MyClassTest {
     @Test
     public void testAdd() {
+        System.out.println(">>> RUNNING MyClassTest");
+
         MyClass myClass = new MyClass();
-        assertEquals(4, myClass.add(1, 2));
+        assertEquals(3, myClass.add(1, 2));
         assertEquals(0, myClass.add(-1, 1));
         assertEquals(0, myClass.add(0, 0));
     }
